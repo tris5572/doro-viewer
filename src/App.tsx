a9728333b045f28b52ccb3787f1a5d74e4180442
tris5572 <68403260+tris5572@users.noreply.github.com>
2026-03-28 10:32:25 +0900

    feat: データ表示を実装

--- src/App.tsx
+++ src/App.tsx
@@ -1,102 +1,46 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "./assets/vite.svg";
-import heroImg from "./assets/hero.png";
-import "./App.css";
+import ReactMap, {
+  FullscreenControl,
+  GeolocateControl,
+  Layer,
+  NavigationControl,
+  ScaleControl,
+  Source,
+} from "react-map-gl/maplibre";
+import "maplibre-gl/dist/maplibre-gl.css";
+import data1 from "./data/1.json";
+import data2 from "./data/2.json";
+import data3 from "./data/3.json";
 
-function App() {
-  const [count, setCount] = useState(0);
+const CONTROL_STYLES: React.CSSProperties = {
+  background: "hsl(0 0% 100% / 0.5)",
+  backdropFilter: "blur(4px)",
+};
 
+export function App() {
   return (
-    <>
-      <section id="center">
-        <div className="hero">
-          <img src={heroImg} className="base" width="170" height="179" alt="" />
-          <img src={reactLogo} className="framework" alt="React logo" />
-          <img src={viteLogo} className="vite" alt="Vite logo" />
-        </div>
-        <div>
-          <h1>Get started</h1>
-          <p>
-            Edit <code>src/App.tsx</code> and save to test <code>HMR</code>
-          </p>
-        </div>
-        <button className="counter" onClick={() => setCount((count) => count + 1)}>
-          Count is {count}
-        </button>
-      </section>
-
-      <div className="ticks"></div>
-
-      <section id="next-steps">
-        <div id="docs">
-          <svg className="icon" role="presentation" aria-hidden="true">
-            <use href="/icons.svg#documentation-icon"></use>
-          </svg>
-          <h2>Documentation</h2>
-          <p>Your questions, answered</p>
-          <ul>
-            <li>
-              <a href="https://vite.dev/" target="_blank">
-                <img className="logo" src={viteLogo} alt="" />
-                Explore Vite
-              </a>
-            </li>
-            <li>
-              <a href="https://react.dev/" target="_blank">
-                <img className="button-icon" src={reactLogo} alt="" />
-                Learn more
-              </a>
-            </li>
-          </ul>
-        </div>
-        <div id="social">
-          <svg className="icon" role="presentation" aria-hidden="true">
-            <use href="/icons.svg#social-icon"></use>
-          </svg>
-          <h2>Connect with us</h2>
-          <p>Join the Vite community</p>
-          <ul>
-            <li>
-              <a href="https://github.com/vitejs/vite" target="_blank">
-                <svg className="button-icon" role="presentation" aria-hidden="true">
-                  <use href="/icons.svg#github-icon"></use>
-                </svg>
-                GitHub
-              </a>
-            </li>
-            <li>
-              <a href="https://chat.vite.dev/" target="_blank">
-                <svg className="button-icon" role="presentation" aria-hidden="true">
-                  <use href="/icons.svg#discord-icon"></use>
-                </svg>
-                Discord
-              </a>
-            </li>
-            <li>
-              <a href="https://x.com/vite_js" target="_blank">
-                <svg className="button-icon" role="presentation" aria-hidden="true">
-                  <use href="/icons.svg#x-icon"></use>
-                </svg>
-                X.com
-              </a>
-            </li>
-            <li>
-              <a href="https://bsky.app/profile/vite.dev" target="_blank">
-                <svg className="button-icon" role="presentation" aria-hidden="true">
-                  <use href="/icons.svg#bluesky-icon"></use>
-                </svg>
-                Bluesky
-              </a>
-            </li>
-          </ul>
-        </div>
-      </section>
-
-      <div className="ticks"></div>
-      <section id="spacer"></section>
-    </>
+    <div id="app">
+      <ReactMap
+        initialViewState={{
+          longitude: 139,
+          latitude: 36,
+          zoom: 8,
+        }}
+        mapStyle="https://tris5572.github.io/map-style/dark/style.json"
+      >
+        <Source id="local" type="geojson" data={data3 as any}>
+          <Layer type="line" paint={{ "line-color": "#c6c299", "line-width": 2 }} />
+        </Source>
+        <Source id="general" type="geojson" data={data2 as any}>
+          <Layer type="line" paint={{ "line-color": "#d19723", "line-width": 2 }} />
+        </Source>
+        <Source id="highway" type="geojson" data={data1 as any}>
+          <Layer type="line" paint={{ "line-color": "#0da344", "line-width": 3 }} />
+        </Source>
+        <ScaleControl style={CONTROL_STYLES} />
+        <NavigationControl style={CONTROL_STYLES} />
+        <FullscreenControl style={CONTROL_STYLES} />
+        <GeolocateControl style={CONTROL_STYLES} />
+      </ReactMap>
+    </div>
   );
 }
-
-export default App;
